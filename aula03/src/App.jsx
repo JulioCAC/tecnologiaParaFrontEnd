01c6bf8083295027c96ebf1bf9453aad6c658321
JulioCAC <juliocac999@gmail.com>
2024-09-11 15:53:18 -0300

    questao 3 resolvida(refatoracao da questao 2)

--- aula03/src/App.jsx
+++ aula03/src/App.jsx
@@ -9,13 +9,13 @@ const App = () => {
       <h1>Questão 1:</h1>
       <EstouConseguindoAprenderReact estouConseguindo = {true}/>
       <EstouConseguindoAprenderReact estouConseguindo = {false}/>
-      <h1>Questão 2:</h1>
+      <h1>Questão 2 e 3:</h1>
       {
         [
-          { nome: 'João', email: 'joao@email.com', curso: 'Engenharia' },
-          { nome: 'Maria', email: 'maria@email.com', curso: 'Medicina' },
-          { nome: 'Lucas', email: 'lucas@email.com', curso: 'Direito' }
-        ].map((aluno) => <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso}/>)
+          { nome: 'João', email: 'joao@email.com', curso: 'Engenharia', media: 8.5 },
+          { nome: 'Maria', email: 'maria@email.com', curso: 'Medicina', media: 6.4 },
+          { nome: 'Lucas', email: 'lucas@email.com', curso: 'Direito', media: 7.0 }
+        ].map((aluno) => <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso} media={aluno.media}/>)
       }
     </div>
   )
--- aula03/src/components/Aluno.jsx
+++ aula03/src/components/Aluno.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Aluno = ({ nome, email, curso }) => {
+const Aluno = ({ nome, email, curso, media }) => {
   return (
     <div>
         <p>Nome: {nome}</p>
         <p>Email: {email}</p>
         <p>Curso: {curso}</p>
+        <p>Media: {media}</p>
+        <p>Status: {media >= 7 ? <strong>APROVADO</strong> : <strong>REPROVADO</strong>}</p>
         <hr/>
     </div>
   )
